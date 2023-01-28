c3fa6f70100b82831f4ee5708b80f374dcc60c50
Dmitry Kalugin <n.demitsuri@gmail.com>
2023-01-28 18:50:31 +0300

    Add footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+import { Typography } from '@alfalab/core-components/typography';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <Typography.Text tag="p" color="tertiary" weight="bold">
+        © ООО «Альфа Фьюче Пипл», 2022
+      </Typography.Text>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/page/Page.tsx
+++ src/components/page/Page.tsx
@@ -1,12 +1,14 @@
-import Header from "../header/Header";
+import Footer from '../footer/Footer';
+import Header from '../header/Header';
 
 function Page() {
-  return (<>
-    <Header />
-    <main>
-      Main
-    </main>
-  </>)
+  return (
+    <>
+      <Header />
+      <main>Main</main>
+      <Footer />
+    </>
+  );
 }
 
 export default Page;
