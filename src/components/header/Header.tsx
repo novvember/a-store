7d050cd832d912b889d544e2bb1ecf8b62d183b2
Dmitry Kalugin <n.demitsuri@gmail.com>
2023-01-28 19:08:07 +0300

    Add link in header to home page

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,18 +4,21 @@ import { Space } from '@alfalab/core-components/space';
 import { BurgerMIcon } from '@alfalab/icons-glyph/BurgerMIcon';
 
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <header className="header">
-      <Typography.TitleResponsive
-        view="large"
-        tag="h1"
-        color="accent"
-        weight="bold"
-      >
-        A-Store
-      </Typography.TitleResponsive>
+      <Link to="/">
+        <Typography.TitleResponsive
+          view="large"
+          tag="h1"
+          color="accent"
+          weight="bold"
+        >
+          A-Store
+        </Typography.TitleResponsive>
+      </Link>
 
       <Button view="ghost">
         <Space direction="horizontal" align="end" size="s">
